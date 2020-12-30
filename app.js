3e12bfd4a2c352bdb41932be750a8043802f8684
belinda <dog83919814@yahoo.com.tw>
2020-12-30 16:56:06 +0800

    feat: add CRUD function

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Record = require('./models/Record.js')
+const bodyParser = require('body-parser')
+
 
 const app = express()
 const POST = 3000
+
+
 
 mongoose.connect('mongodb://localhost/expense_tracker', {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
@@ -15,13 +19,49 @@ db.once('open',() => {
 
 app.engine('handlebars',exphbs({defaultLayout : 'main'}))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/', (req, res) => {  
   Record.find()
   .lean()
   .then(record => res.render('index',{record}))
-  .then(error => console.log(error))
+  .catch(error => console.log(error))
 })
+app.get('/record/new', (req, res) => {  
+  res.render('new')
+})
+app.post('/record', (req, res) => {  
+  return Record.create(req.body)
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+})
+app.get('/record/:id/edit', (req, res) => {  
+  const id = req.params.id
+  Record.findById(id)
+  .lean()
+  .then(record => res.render('edit',{record}))
+  .catch(error => console.log(error))
+})
+app.post('/record/:id', (req, res) => {  
+  const id = req.params.id
+  return Record.findById(id)
+        .then(record => {
+          record = Object.assign(record, req.body)
+          return record.save()
+        })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+})
+app.post('/record/:id/delete', (req, res) => {  
+  const id = req.params.id
+  return Record.findById(id)
+        .then(record => record.remove())
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+})
+
+
+
 
 app.listen(POST, () => {
   console.log(`The Express Tracker Web is running on http://localhost:${POST}`)
