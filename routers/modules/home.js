8b62a1b54184564254b0c35e09c1e617afc6f9a8
belinda <dog83919814@yahoo.com.tw>
2020-12-31 14:44:29 +0800

    feat: add totalAmount

--- routers/modules/home.js
+++ routers/modules/home.js
@@ -3,9 +3,13 @@ const router = express.Router()
 const Record = require('../../models/Record')
 
 router.get('/', (req, res) => {  
+  let sum = 0
   Record.find()
   .lean()
-  .then(record => res.render('index',{record}))
+  .then(record => {
+    record.forEach(record => sum += record.amount)
+    res.render('index',{record, sum})
+  })
   .catch(error => console.log(error))
 })
 
--- routers/modules/search.js
+++ routers/modules/search.js
@@ -3,12 +3,14 @@ const router = express.Router()
 const Record = require('../../models/Record')
 
 router.get('/', (req, res) => {  
+  let sum = 0
   const selectedCategory = req.query.category
   Record.find()
   .lean()
   .then(records => {    
     const record = records.filter(record => record.category === selectedCategory )
-    res.render('index',{record, selectedCategory})
+    record.filter( record => sum += record.amount)
+    res.render('index',{record, selectedCategory, sum})
   })
   .catch(error => console.log(error))
 })
