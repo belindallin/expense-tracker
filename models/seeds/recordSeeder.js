83c25e0bbb312841084736d6ae543013631579e0
belinda <dog83919814@yahoo.com.tw>
2020-12-31 11:13:56 +0800

    feat: modify data's type

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -4,31 +4,31 @@ const db = require('../../config/mongoose')
 db.once('open',() => {
   Record.create({
     name : '洗面乳',
-    date : '10/10/2020',
+    date : '2020-10-10',
     category : '家居物業',
     amount : 500,
   },
   {
     name : '午餐',
-    date : '10/13/2020',
+    date : '2020-10-30',
     category : '餐飲食品',
     amount : 50,
   },
   {
     name : '高鐵',
-    date : '11/10/2020',
+    date : '2020-11-10',
     category : '交通出行',
     amount : 1350,
   },
   {
     name : '電影',
-    date : '12/15/2020',
+    date : '2020-12-15',
     category : '休閒娛樂',
     amount : 500,
   },
   {
-    name : 'iphone 12',
-    date : '12/31/2020',
+    name : 'iphone12',
+    date : '2020-12-31',
     category : '其他',
     amount : 28500,
   })
